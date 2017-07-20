a3e38fb6e36a0a28c5e2aca4751c4d13306e1da6
Lucas Moura <lucasleandrodemoura@gmail.com>
2017-07-20 15:58:15 -0300

    Incremento do controlador principal, função de submit na base

--- GesProjeto/application/core/MY_Controller.php
+++ GesProjeto/application/core/MY_Controller.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+/**
+ * Controladora principal
+ * @author Lucas Leandro de Moura <lucasleandrodemoura@gmail.com:
+ * @data 2017-07-20
+ */
 class MY_Controller extends CI_Controller {
 
     private $prefixo = "";
@@ -17,11 +21,18 @@ class MY_Controller extends CI_Controller {
             }
         }
         
+        /**
+         * Alimenta o titulo do controlador
+         * @param type $titulo
+         */
         function setTitulo($titulo) {
             $this->titulo = $titulo;
         }
 
-                
+        /**
+         * Alimenta a tabela que ser[a usada no sistema
+         * @param type $tabela
+         */
         function setTabela($tabela) {
             $this->tabela = $tabela;
         }
@@ -62,15 +73,23 @@ class MY_Controller extends CI_Controller {
          * @return \Campos Array contendo todos os campos necessários para o cadastro ou edição
          */
         private function camposCadastro($indice=null){
-            
+            $cadastros = new Cadastros();
             $info["action"] = $this->tabela."/submit";
             $info["titulo"] = "Cadastro de ".$this->titulo;
             $info["voltar"] = $this->tabela;
-            if($indice){
+            if(sizeof($indice)>0){
+                $link = "";
+                foreach ($indice as $key=>$value){
+                    if($link!=""){
+                        $link.="&";
+                    }
+                    $link.=$key."=".$value;
+                }
                 $info["botao_editar"] = TRUE;
-                $info["action"] = $this->tabela."/submit/".$indice;
+                $info["action"] = $this->tabela."/submit?".$link;
                 $info["botao_excluir"] = FALSE;
                 $info["botao_inserir"] = FALSE;
+                $cadastros->setEditar($indice);
             }
             else{
                 $info["botao_editar"] = FALSE;
@@ -80,9 +99,6 @@ class MY_Controller extends CI_Controller {
             
        
             
-            $cadastros = new Cadastros();
-            print_r($indice);
-            $cadastros->setEditar($indice);
             
             $info["dados"] = $cadastros->xCadastro($this->prefixo."".$this->tabela);
             
@@ -99,8 +115,7 @@ class MY_Controller extends CI_Controller {
             $this->autentica();
             $tabela = $this->prefixo."".$this->tabela;
             $indice = $this->input->get();
-            print_r($indice);
-            die();
+            
             $info = $this->camposCadastro($indice);
             
             
@@ -112,49 +127,72 @@ class MY_Controller extends CI_Controller {
             $this->load->view("Includes/footer");
         }
         
-        
-        
-        public function submit($codigo_caminhoes=null){
+        /**
+         * Controlador responsável em submter formulários
+         * @author Lucas Leandro de Moura
+         */
+        public function submit(){
             $this->autentica();
-            $this->load->library('form_validation');
+            $filtros = $this->input->get();
             
-            //Validando campos
-            $this->form_validation->set_rules('placa', 'Placa', 'required');
-            $this->form_validation->set_rules('descricao', 'Descrição', 'required');
-            $this->form_validation->set_rules('capacidade', 'Capacidade', 'required');
-            $this->form_validation->set_message('rule', 'Error Message');
             
-            if ($this->form_validation->run() == FALSE){
-                
-                $info = $this->camposCadastro();
-                $this->load->view("Includes/header_nav");
-                $menus = new Menus();
-                $dataMenu["menus"] = $menus->getMenus();
-                $this->load->view("Includes/menu",$dataMenu);
-                $this->load->view("record",$info);
-                $this->load->view("Includes/footer");
-            }
-            else{
                //Realiza a rotina de gravação no banco
-                $data["placa"] = $this->input->post("placa");
-                $data["descricao"] = $this->input->post("descricao");
-                $data["capacidade"] = $this->input->post("capacidade");
-                $data["ativo"] = $this->input->post("ativo");
-                $data["codigo_sistema"] = $this->input->post("codigo_sistema");
-                
-                if($codigo_caminhoes){
-                    $this->db->update($this->prefixo."caminhoes",$data,"codigo_caminhoes = ".$codigo_caminhoes);
-                    redirect("caminhoes");
-                }else{
-                    $this->db->insert($this->prefixo."caminhoes",$data);
-                    redirect("caminhoes");
+                $campos_formulario = $this->input->post();
+                //Pega apenas o que for campos e monta nos registros
+                foreach($campos_formulario as $key=>$value){
+                   if($key!="btn_voltar"
+                           && $key!="btn_editar"
+                           && $key!="btn_excluir"
+                           && $key!="btn_inserir"){
+                        $data[$key] = strtoupper($this->input->post($key));
+                   }
                 }
                 
-            }
+                
+                if(sizeof($filtros)>0){
+                    
+                    $this->before_update();
+                    foreach($filtros as $key_filtro=>$value_filtro){
+                        $this->db->where($key_filtro,$value_filtro);
+                    }
+                    $this->db->update($this->prefixo."".$this->tabela,$data);
+                    $this->after_update();
+                    
+                }else{
+                    
+                    $this->before_insert();
+                    $this->db->insert($this->prefixo."".$this->tabela,$data);
+                    $this->after_insert();
+                }
+                
+                redirect($this->tabela);
                
         }
         
+        /**
+         * Função para instruções depois de inclusões
+         * @author Lucas Leandro de Moura
+         */
+        protected function after_insert(){
+            
         }
+        
+        /**
+         * Função para instruções antes de inclusões
+         */
+        protected function before_insert(){
+            
+        }
+
+        protected function before_update() {
+
+        }
+
+        protected function after_update() {
+
+        }
+
+}
             
             
 
