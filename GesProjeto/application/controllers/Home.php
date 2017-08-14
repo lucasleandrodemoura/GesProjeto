381710eb802d84a6e98f456bb59a4da0b7617a63
Lucas <lucasleandrodemoura@gmail.com>
2017-08-14 20:16:00 -0300

    Alterações mostradas para o Sérgio

--- GesProjeto/application/controllers/Home.php
+++ GesProjeto/application/controllers/Home.php
@@ -35,6 +35,31 @@ class Home extends CI_Controller {
         
     }
     
+    
+    function cadastro(){
+        $this->load->view("Includes/header");
+        $this->load->view("Includes/header_nav");
+        $data["dados"] = $this->db->get("clientes")->result();
+        
+        $this->load->view("cliente_cadastro",$data);
+        $this->load->view("Includes/footer");
+    }
+    
+    
+    function cadastrar(){
+        
+        $dados["nome"] = $this->input->post("nome");
+        $dados["cidade"]= $this->input->post("cidade");
+        $dados["estado"]= $this->input->post("estado");
+        
+     
+        $this->db->insert("clientes",$dados);
+        
+        redirect("home/cadastro");
+     
+        
+    }
+    
    
 }
             
