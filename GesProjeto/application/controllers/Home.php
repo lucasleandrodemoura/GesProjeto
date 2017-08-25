5b2130a5c1bc03474dec8396c4f988cd1ff41f60
Lucas Leandro de Moura <lucasleandrodemoura@gmail.com>
2017-08-24 22:06:40 -0300

    Criação de barra de navegação para menus

--- GesProjeto/application/controllers/Home.php
+++ GesProjeto/application/controllers/Home.php
@@ -11,32 +11,13 @@ class Home extends MY_Controller {
         $this->load->view("Includes/footer");
     }
     
-    
-    function cadastro(){
+    function teste(){
         $this->load->view("Includes/header");
         $this->load->view("Includes/header_nav");
-        $data["dados"] = $this->db->get("clientes")->result();
-        
-        $this->load->view("cliente_cadastro",$data);
+        $this->load->view("home");
         $this->load->view("Includes/footer");
     }
     
-    
-    function cadastrar(){
-        
-        $dados["nome"] = $this->input->post("nome");
-        $dados["cidade"]= $this->input->post("cidade");
-        $dados["estado"]= $this->input->post("estado");
-        
-     
-        $this->db->insert("clientes",$dados);
-        
-        redirect("home/cadastro");
-     
-        
-    }
-    
-   
 }
             
 
--- GesProjeto/application/views/Includes/header_nav.php
+++ GesProjeto/application/views/Includes/header_nav.php
@@ -1,32 +1,24 @@
-  
-    <div class="navbar navbar-default navbar-fixed-top" role="nabigation">
-        <div class="container-fluid" style="margin-left: 0px;">
-            
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            
-            <div class="navbar-header">
-                <a href="/" class="navbar-brand">
-                    Projetos
-                </a>
-            </div>
-            
-            <div class="navbar-collapse collapse">
-                
-            </div>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Projetos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="<?=base_url()?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cadastros
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Projetos</a>
         </div>
-    </div>
-    
-                
-                
-    
-        
-                
-
-  
-    
-  
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Meus dados</a>
+      </li>
+    </ul>
+  </div>
+</nav>
