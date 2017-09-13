8694cc7049cc28960a15debce00317e5da86f63a
Lucas Moura <lucasleandrodemoura@gmail.com>
2017-09-12 22:21:45 -0300

    Alterado a VIEW de list default

--- GesProjeto/application/views/default_list.php
+++ GesProjeto/application/views/default_list.php
@@ -1,7 +1,15 @@
 
 <div class="container-fluid">
+    <?php if($titulo){?>
     <div class="row">
-          <div class="col-lg-12">
+        <div class="col-lg-12">
+            <h3><?=$titulo?></h3>
+        </div>
+    </div>
+    <?php } ?>
+    <div class="row">
+        
+          <div class="col-lg-12 table-responsive">
                <?php if($tabela){?>
                 <?=$tabela?>
               <?php } ?>
