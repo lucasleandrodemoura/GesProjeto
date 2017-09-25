49c0e48103109b659a12c6e6e52cf591ea1c03d8
Lucas Moura <lucasleandrodemoura@gmail.com>
2017-09-25 10:52:49 -0300

    Alterado a pasta de cache do sistema

--- GesProjeto/application/config/config.php
+++ GesProjeto/application/config/config.php
@@ -213,7 +213,7 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 | system/cache/ folder.  Use a full server path with trailing slash.
 |
 */
-$config['cache_path'] = '/var/www/html/system/cache/';
+$config['cache_path'] = '/var/www/gestaoprojetos/application/cache/';
 
 /*
 |--------------------------------------------------------------------------
